0847dc3fde62938eee1ea0567910aa96b49f8c7e
maxim <maxim.weewauters@student.howest.be>
2021-04-19 11:41:07 +0200

    bugfix

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ var animation2 = bodymovin.loadAnimation({
   renderer: "svg",
   loop: true,
   autoplay: false,
-  path: "./assets/animations/education.json",
+  path: "./assets/Comp1.json",
 });
 
 const animationcontainer2 = document.querySelector(".animation_2");
